551d684370293b633bbb91dc5bd51d3b3df0dc8d
Tobias Schumacher <tobias.schumacher1@rwth-aachen.de>
2018-02-23 09:32:12 +0100

    Autotest updates

--- tests/testthat/test_binarize.R
+++ tests/testthat/test_binarize.R
@@ -4,21 +4,32 @@ context("Binarize")
 ## set up data sets and predictor matrix parameter for tests
 pred_matrix <- (1 - diag(1, 9))
 pred_matrix[6,7] <- 0
+boys_groups1 <- c(0,0,0,0,0,1,1,0,0)
+boys_groups2 <- c(0,0,0,0,0,2,1,0,0)
+boys_weights <- c(1,1,1,1,1,4,5,1,1)
 boys_bin1 <- mice.binarize(boys_data)
 boys_bin2 <- mice.binarize(boys_data, include_observed = TRUE, pred_matrix = pred_matrix)
 boys_bin3 <- mice.binarize(boys_data, cols = "gen")
+boys_bin4 <- mice.binarize(boys_data, blocks = c("gen","phb"), weights = c(2,3))
+boys_bin5 <- mice.binarize(boys_data, blocks = boys_groups1, weights = c(2,3))
+boys_bin6 <- mice.binarize(boys_data, blocks = boys_groups2)
+boys_bin7 <- mice.binarize(boys_data, blocks = boys_groups1, weights = boys_weights)
+boys_bin8 <- mice.binarize(boys_data, weights = boys_weights)
 
 
 ## test input checks
 test_that("Test input checks",
           {
             expect_error(mice.binarize(mammal_data), "Data doesn't contain any non-binary categorical attributes with unobserved values.")
-            expect_error(mice.binarize(mammal_data, cols = "gt", "Not all columns in argument 'cols' are non-binary factors "))
+            expect_error(mice.binarize(mammal_data, cols = "gt"), "Not all columns in argument 'cols' are non-binary factors.")
             expect_error(mice.binarize(mammal_data, cols = "ct"), "Argument 'cols' contains invalid column names.")
             expect_error(mice.binarize(mammal_data, cols = c(10,20)), "Argument 'cols' contains an invalid column index.")
             expect_error(mice.binarize(boys_data, cols = c("gen","gen")), "Argument 'cols' contains duplicates.")
             expect_error(mice.binarize(boys_data, include_ordered = FALSE), "Data doesn't contain any non-binary unordered categorical attributes with unobserved values.")
-            expect_error(mice.binarize(boys_data, pred_matrix = diag(1,9), "Diagonal elements of input predictor matrix have to be zero."))
+            expect_error(mice.binarize(boys_data, pred_matrix = diag(1,9)), "Diagonal elements of input predictor matrix have to be zero.")
+            expect_error(mice.binarize(boys_data, weights = c(2,3)), "Input argument 'weights' must not be in list format if no blocks have been specified.")
+            expect_error(mice.binarize(boys_data, blocks = boys_groups, weights = c(2,3,4)), "Length of weights tuple 1 does not match length of column block 1..")
+            expect_error(mice.binarize(boys_data, blocks = c(0,0,0,0,0,2,3,0,0)), "Argument 'blocks' contains invalid group indices.")
           })
 
 
@@ -28,4 +39,9 @@ test_that("Test functionality",
             expect_equal_to_reference(boys_bin1, file = "boys_bin1.rds")
             expect_equal_to_reference(boys_bin2, file = "boys_bin2.rds")
             expect_equal_to_reference(boys_bin3, file = "boys_bin3.rds")
+            expect_equal_to_reference(boys_bin4, file = "boys_bin4.rds")
+            expect_equal_to_reference(boys_bin5, file = "boys_bin5.rds")
+            expect_equal_to_reference(boys_bin6, file = "boys_bin6.rds")
+            expect_equal_to_reference(boys_bin7, file = "boys_bin7.rds")
+            expect_equal_to_reference(boys_bin8, file = "boys_bin8.rds")
           })
--- tests/testthat/test_multpmm.R
+++ tests/testthat/test_multpmm.R
@@ -8,6 +8,10 @@ set.seed(4711)
 mammal2 <- mammal_data
 mammal2[!is.na(mammal2$sws),"pi"] <- 2L
 
+# minor helper variables for error checks
+mammal_blocks_fail1 <- c(0,0,0,1,1,0,0,2,2,0,0,0)
+mammal_blocks_fail2 <- c(1,0,0,1,1,0,2,2,0,0,0)
+
 ## load other test data
 mids_nh <- readRDS("mids_nh.rds")
 mids_mammal <- readRDS("mids_mammal.rds")
@@ -23,6 +27,9 @@ post_nh1 <- mice.post.matching(mids_nh, distmetric = "euclidian")
 # apply match_vars and further parameters
 post_nh2 <- mice.post.matching(mids_nh, donors = 2L, distmetric = "manhattan", matchtype = 2L, eps = 0.0002, weights = c(1,5), match_vars = "age")
 
+# apply blocks and weights using with vector notation
+post_nh3 <- mice.post.matching(mids_nh, blocks = c(0,1,1,0), weights = c(1,2,3,4), distmetric = "euclidian")
+
 
 ## test input checks
 test_that("Test input checks",
@@ -33,6 +40,8 @@ test_that("Test input checks",
             expect_error(mice.post.matching(mids_mammal, blocks = c("sws","gt")), "Not all tuples in given columns are either blockwise NA or blockwise non-NA.")
             expect_error(mice.post.matching(mids_mammal, blocks = c(10,20)), "Argument 'blocks' contains a tuple with out-of-bounds column numbers.")
             expect_error(mice.post.matching(mids_mammal, blocks = list(c("sws","ps"), c("sws","ps"))), "Argument 'blocks' contains duplicate columns among its elements.")
+            expect_error(mice.post.matching(mids_mammal, blocks = mammal_blocks_fail1), "Argument 'blocks' has invalid length.")
+            expect_error(mice.post.matching(mids_mammal, blocks = mammal_blocks_fail2), "Argument 'blocks' contains a tuple with a column index that is not in the visit sequence.")
             expect_error(mice.post.matching(mids_fail1), "There are no column tuples with identical missing data patterns and valid imputation methods.")
             expect_error(mice.post.matching(mids_fail2, blocks = c("sws","ps"), match_vars = "pi"), "Column tuple*")
           })
@@ -43,4 +52,5 @@ test_that("Test functionality",
           {
             expect_equal_to_reference(post_nh1, file = "post_nh1.rds")
             expect_equal_to_reference(post_nh2, file = "post_nh2.rds")
+            expect_equal_to_reference(post_nh3, file = "post_nh3.rds")
           })
